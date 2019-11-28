3678c0914a9489169312d6af3036699878e5ff2b
rakibgalib <rakibhossaingalib@gmail.com>
2019-11-28 09:39:05 +0600

    Code Removed From Master Branch

--- app/assets/pages/datatables.init.js
+++
@@ -1,9 +0,0 @@
-/*
- Template Name: Admiry - Bootstrap 4 Admin Dashboard
- Author: Themesbrand
- File: Datatable js
- */
-
-$(document).ready(function () {
-    $("#datatable").DataTable();
-});
--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var Bankstatment = artifacts.require("./BankstatmentContract.sol");
-
-module.exports = (deployer, network, accounts) => {
-    let deployAddress = accounts[0];
-    deployer.deploy(Bankstatment, { from: deployAddress });
-};
